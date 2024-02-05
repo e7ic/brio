db9fb0b87affbc2c9749fe0f73673ecddae33147
e7ic <i@coooooc.com>
2024-02-05 18:14:13 +0800

    chore: Extracted motion components

--- components/motion-panel.js
+++ components/motion-panel.js
@@ -12,6 +12,7 @@ import {
     CircleIcon,
     VectorLineIcon, EyeClosedIcon, LockIcon, FolderIcon, ArrowRightFillIcon
 } from "@/components/icons";
+import {motion} from "framer-motion";
 import {useDraggable} from "@/hooks/use-draggable";
 import {Input} from "@/components/ui/input";
 import {cn} from "@/lib/utils";
@@ -20,7 +21,6 @@ import {useDesignStore} from "@/store";
 export function MotionPanel({nodes}) {
     const [width, startDrag] = useDraggable(288, 224, 448);
     const resizableRef = useRef(null);
-    const {basic, currentId, setCurrentId, update} = useDesignStore()
 
     const handleMouseDown = (e) => {
         const startRect = resizableRef.current.getBoundingClientRect();
@@ -36,77 +36,7 @@ export function MotionPanel({nodes}) {
             </div>
             <div className="w-full h-full pb-4 overflow-y-auto">
                 {
-                    nodes?.map((node, index) => <div
-                        key={index}
-                        onClick={() => {
-                            setCurrentId(node.id)
-                        }}
-                        className={cn("group flex items-center px-4 space-x-2 w-full h-8 border border-transparent", {
-                            "hover:border-border": currentId !== node.id && node.visible,
-                            "bg-muted": currentId === node.id
-                        })}>
-                        <Button variant="ghost" className="flex-shrink-0 p-0 w-4 h-fit hover:bg-transparent">
-                            {node.type === "GROUP" && <ArrowRightFillIcon className="w-full h-4 fill-muted-foreground/50"/>}
-                        </Button>
-                        <Button variant="ghost" className="p-0 h-fit" onDoubleClick={() => {
-                            console.log("position")
-                        }}>
-                            <LayerIcon type={node.type}
-                                       className={cn("flex-shrink-0 w-4 h-4 fill-muted-foreground/50", {
-                                           "group-hover:fill-muted-foreground": node.visible,
-                                           "fill-muted-foreground": currentId === node.id
-                                       })}/>
-                        </Button>
-                        <EditableElement value={node.name} onChange={async e => {
-                            const payload = basic?.nodes?.map((item, i) => {
-                                if (i === index) {
-                                    item.name = e
-                                }
-                                return item
-                            })
-                            await update({
-                                nodes: payload
-                            })
-                        }}>
-                                <span
-                                    className={cn("block tex text-xs cursor-default truncate select-none", {"text-muted-foreground/50": !node.visible})}>{node.name}</span>
-                        </EditableElement>
-                        <div className="hidden flex-shrink-0 space-x-2 group-hover:flex layer-action">
-                            <Button variant="ghost" className="p-0 h-full" onClick={async () => {
-                                const payload = basic?.nodes?.map((item, i) => {
-                                    if (i === index) {
-                                        item.locked = !item.locked
-                                    }
-                                    return item
-                                })
-                                await update({
-                                    nodes: payload
-                                })
-                            }}>
-                                {node.locked ? <LockIcon
-                                        className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/> :
-                                    <UnlockIcon
-                                        className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/>}
-                            </Button>
-                            <Button variant="ghost" className="p-0 h-full" onClick={async () => {
-                                const payload = basic?.nodes?.map((item, i) => {
-                                    if (i === index) {
-                                        item.visible = !item.visible
-                                    }
-                                    return item
-                                })
-                                await update({nodes: payload})
-                            }}>
-                                {
-                                    node.visible ?
-                                        <EyeIcon
-                                            className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/> :
-                                        <EyeClosedIcon
-                                            className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/>
-                                }
-                            </Button>
-                        </div>
-                    </div>)
+                    nodes?.map(node => <LayerCell key={node.id} node={node} />)
                 }
             </div>
             <div className="absolute top-0 right-0 w-1 h-full select-none cursor-ew-resize bg-transparent"
@@ -132,6 +62,86 @@ const LayerIcon = ({type, ...props}) => {
         case "GROUP":
             return <FolderIcon {...props} />
     }
+}
+
+const LayerCell = ({ node }) => {
+    const {basic, currentId, setCurrentId, update} = useDesignStore()
+    return <motion.div
+        onClick={e => {
+            e.preventDefault()
+            setCurrentId(node.id)
+        }}
+        className={cn("group flex items-center px-4 space-x-2 w-full h-8 border border-transparent bg-background", {
+            "hover:border-border": currentId !== node.id && node.visible,
+            "bg-muted": currentId === node.id
+        })}
+        drag="y"
+        dragConstraints={{top: 0, right: 0, bottom: 0, left: 0}}
+        dragMomentum={false}
+        dragElastic={1}
+    >
+        <Button variant="ghost" className="flex-shrink-0 p-0 w-4 h-fit hover:bg-transparent">
+            {node.type === "GROUP" && <ArrowRightFillIcon className="w-full h-4 fill-muted-foreground/50"/>}
+        </Button>
+        <Button variant="ghost" className="p-0 h-fit" onDoubleClick={() => {
+            console.log("position")
+        }}>
+            <LayerIcon type={node.type}
+                       className={cn("flex-shrink-0 w-4 h-4 fill-muted-foreground/50", {
+                           "group-hover:fill-muted-foreground": node.visible,
+                           "fill-muted-foreground": currentId === node.id
+                       })}/>
+        </Button>
+        <EditableElement value={node.name} onChange={async e => {
+            const payload = basic?.nodes?.map(item => {
+                if (item.id === currentId) {
+                    item.name = e
+                }
+                return item
+            })
+            await update({
+                nodes: payload
+            })
+        }}>
+                                <span
+                                    className={cn("block tex text-xs cursor-default truncate select-none", {"text-muted-foreground/50": !node.visible})}>{node.name}</span>
+        </EditableElement>
+        <div className="hidden flex-shrink-0 space-x-2 group-hover:flex layer-action">
+            <Button variant="ghost" className="p-0 h-full" onClick={async () => {
+                const payload = basic?.nodes?.map(item => {
+                    if (item.id === currentId) {
+                        item.locked = !item.locked
+                    }
+                    return item
+                })
+                await update({
+                    nodes: payload
+                })
+            }}>
+                {node.locked ? <LockIcon
+                        className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/> :
+                    <UnlockIcon
+                        className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/>}
+            </Button>
+            <Button variant="ghost" className="p-0 h-full" onClick={async () => {
+                const payload = basic?.nodes?.map(item => {
+                    if (item.id === currentId) {
+                        item.visible = !item.visible
+                    }
+                    return item
+                })
+                await update({nodes: payload})
+            }}>
+                {
+                    node.visible ?
+                        <EyeIcon
+                            className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/> :
+                        <EyeClosedIcon
+                            className={cn("w-3 h-3", {"fill-muted-foreground/50": !node.visible})}/>
+                }
+            </Button>
+        </div>
+    </motion.div>
 }
 
 const EditableElement = ({value, children, onChange}) => {
